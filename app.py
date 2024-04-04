5ddd39a02e566f9511aeed0e10ce646f9a10be71
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2024-04-04 00:58:47 -0600

    htmx

--- app.py
+++ app.py
@@ -2,50 +2,110 @@
 
 import json
 import re
-import sys
 
 from flask import Flask
 from flask import request
 from flask import render_template
-# from flask import url_for
+from flask import Response
+import requests
 import udar
 
 Sharoff_lem_freq_dict = udar.features.features._get_Sharoff_lem_freq_dict()
 
 # The following sentence is used if the user enters nothing
 EXAMPLE_SENT = 'ПРИМЕР: Ана не хочит мить пасуду в кафетерие, но мы скозали эй, что обизателно надо.'
+DEFAULT_UI_LANG = "eng"
+EXPLANATION_BASE_URL = "https://reynoldsnlp.github.io/rus_grammar_explanations/html/"
 
 app = Flask(__name__)
 application = app  # our hosting requires `application` in passenger_wsgi
 
+# app.jinja_env.add_extension('jinja2.ext.debug')  # enables <pre>{% debug %}</pre>
 
-# passenger sets '/' to be the path registered in cPanel
-@app.route('/', methods=['GET', 'POST'])
-def freq_form_post():
-    """Build Russian frequency sorter page."""
-    if request.method == 'GET':
-        return render_template("entry_form.html")
-    elif request.method == 'POST':
-        text = request.form['text'].strip() or EXAMPLE_SENT
-        doc = udar.Document(text, analyze_L2_errors=True, disambiguate=True)
-        text_html = doc2html(doc)
-        return render_template('output.html', text_html=text_html)
+with open("l10n.json") as f:
+    l10n = json.load(f)
+l10n_lang = DEFAULT_UI_LANG
+
+
+@app.get('/')
+def index():
+    lang = request.args.get("rumor-l10n-lang", l10n_lang)
+    return render_template("index.html", l10n=l10n[lang])
+
+
+@app.get('/corrected/<show>/<corrected_wordform>')
+def corrected(show, corrected_wordform):
+    if show == "show":
+        show = "hide"
+    elif show == "hide":
+        show = "show"
+    return render_template("corrected.html",
+                           show=show,
+                           corrected_wordform=corrected_wordform)
+
+
+@app.get('/entry-form')
+def entry_form():
+    return render_template('entry-form.html')
+
+
+@app.post('/error-list')
+def error_list():
+    lang = request.args.get("rumor-l10n-lang", l10n_lang)
+    error_json = request.form.get('error-list', "").replace('`', '"')  # TODO ticks are brittle
+    tok_id = request.form.get('id', "")
+    orig = request.form.get('orig', "")
+    if error_json:
+        errors = json.loads(error_json)
+        return render_template("error-list.html",
+                               errors=errors,
+                               l10n=l10n[lang],
+                               l10n_lang=lang,
+                               orig=orig,
+                               tok_id=tok_id)
+    else:
+        return "No errors"
+
+
+@app.get("/explanation/<lang>/<L2_err_tag>")
+def explanation(lang, L2_err_tag):
+    resp = requests.get(f"{EXPLANATION_BASE_URL}/{lang}/{L2_err_tag}.html")
+    return resp.text
+
+
+@app.get('/l10n/<lang>')
+def set_l10n_lang(lang):
+    global l10n_lang
+    l10n_lang = lang
+    resp = Response(json.dumps(l10n[lang]))
+    resp.headers['HX-Trigger'] = json.dumps({"setLang": {"newLang": lang,
+                                                         "l10n": l10n[lang]}})
+    resp.headers['Set-Cookie'] = f"rumor-l10n-lang={lang}"
+    return resp
+
+
+@app.post('/process-input')
+def process_input():
+    lang = request.cookies.get("rumor-l10n-lang", l10n_lang)
+    text = request.form['text'].strip() or EXAMPLE_SENT
+    doc = udar.Document(text, analyze_L2_errors=True, disambiguate=True)
+    text_html = doc2html(doc)
+    return render_template('output.html', text_html=text_html, l10n=l10n[lang])
 
 
 def doc2html(doc):
     """Convert udar.Document to html marking L2 errors using <span> tags."""
+    return re.sub(r' [.?!]', '', ' '.join(tok2html(i, tok)
+                                          for i, tok in enumerate(doc)))
 
-    return re.sub(r' [.?!]', '', ' '.join(tok2html(tok) for tok in doc))
 
-
-def tok2html(tok):
+def tok2html(i, tok):
     """Convert udar.Token to html <span>.
 
-    The data-err attribute has the following structure:
+    The hx-vals attribute has the following structure:
         {lemma: {uniq_tag(s): [[err1, err2], corrected],
                  uniq_tag(s): [[err1], corrected],
          lemma2: ...}
-
     """
     if tok.is_L2_error():
         err_list = []
@@ -70,12 +130,18 @@ def tok2html(tok):
                              'tags': [tag.name for tag in reading.grouped_tags],
                              'uniq_tags': uniq_tags,
                              'L2_error_tags': L2_error_tags,
+                             'orig': tok.text,
                              'corrected': corrected,
                              'freq': Sharoff_lem_freq_dict.get(' '.join(reading.lemmas), 0)})
         # title = f'''title="{' '.join(all_L2_error_tags)} {corrected}" '''
-        err_json = json.dumps(err_list, ensure_ascii=False)
-        print(err_json)
-        return f'''\n<a class="err has-text-link" data-errs='{err_json}' onclick="click_err(this)">{tok.text}</a>'''
+        err_json = json.dumps(err_list, ensure_ascii=False).replace('"', '`')
+        return f'''\n<a id="tok-{i}"
+                        class="err has-text-link"
+                        hx-post="/error-list"
+                        hx-target="#error-list"
+                        hx-on::before-request="highlightToken(this)"
+                        hx-vals='{{"error-list": "{err_json}", "id": "tok-{i}", "orig": "{tok.text}"}}'
+                        >{tok.text}</a>'''
     else:
         return tok.text
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,191 +1,59 @@
-l10n_lang = "eng";
+setLang = function (newLang, l10n) {
+	langTags = document.getElementsByClassName("lang");
+	for (let i in langTags) {
+		langTags[i]?.classList?.add("is-light");
+	}
+	newLangTags = document.getElementsByClassName(newLang);
+	newLangTags[0]?.classList?.remove("is-light");
 
-l10n = {
-	"eng": {
-		"errors": {
-			"a2o": "o→a",
-			"e2je": "e→э",
-			"FV": "no fill vowel",
-			"H2S": "ъ→ь",
-			"i2j": "й→и",
-			"i2y": "ы→и",
-			"ii": "ие→ии",
-			"Ikn": "и→е/я/а",
-			"j2i": "и→й",
-			"je2e": "э→е",
-			"NoFV": "add fill vowel",
-			"NoGem": "add double letter",
-			"NoSS": "add ь",
-			"o2a": "a→o",
-			"Pal": "keep consonant soft",
-			"sh2shch": "щ→ш",
-			"shch2sh": "ш→щ",
-			"ski": "ский→ски",
-			"SRo": "о→е",
-			"SRy": "ы→и",
-			"y2i": "и→ы",
-			"prijti": "прийти",
-			"revIkn": "е/я/а→и",
-			"Gem": "no double letter",
-		},
-		"does_not_exist": "does not exist in Russian. Did you mean one of these?",
-		"tbl_headers": {
-			0: "Dictionary<br>form",
-			1: "Error(s)",
-			2: "Corrected to...<br>(hover/tap to see)",
-		},
-		"restart": "Start over",
-		"powered": "Powered by",
-	},
-
-	"nob": {
-		"errors": {
-			"a2o": "o→a",
-			"e2je": "e→э",
-			"FV": "ingen flyktig vokal",
-			"H2S": "ъ→ь",
-			"i2j": "й→и",
-			"i2y": "ы→и",
-			"ii": "ие→ии",
-			"Ikn": "и→е/я/а",
-			"j2i": "и→й",
-			"je2e": "э→е",
-			"NoFV": "legg til en flyktig vokal",
-			"NoGem": "legg til en dobbelt bokstav",
-			"NoSS": "legg til ь",
-			"o2a": "a→o",
-			"Pal": "gjør konsonanten bløt",
-			"sh2shch": "щ→ш",
-			"shch2sh": "ш→щ",
-			"ski": "ский→ски",
-			"SRo": "о→е",
-			"SRy": "ы→и",
-			"y2i": "и→ы",
-			"prijti": "прийти",
-			"revIkn": "е/я/а→и",
-			"Gem": "ingen dobbelt bokstav",
-		},
-		"does_not_exist": "finnes ikke på russisk. Mente du en av disse?",
-		"tbl_headers": {
-			0: "Ordbokform",
-			1: "Feil",
-			2: "korrigert til...<br>hold markøren over eller klikk)",
-		},
-		"restart": "Begynn på nytt",
-		"powered": "Drevet av",
-	},
-}
-
-
-set_lang = function (new_lang) {
-	l10n_lang = new_lang;
-
-	lang_tags = document.getElementsByClassName("lang");
-	for (var i = 0; i < lang_tags.length; i++) {
-		lang_tags[i].classList.add("is-light");
+	tblHeaders = document.getElementsByTagName("th");
+	for (let i in tblHeaders) {
+		tblHeaders[i].innerHTML = l10n["tbl_headers"][i];
 	}
 
-	new_lang_tags = document.getElementsByClassName(new_lang);
-	new_lang_tags[0].classList.remove("is-light");
-
-	tbl_headers = document.getElementsByTagName("th");
-	for (var i = 0; i < tbl_headers.length; i++) {
-		tbl_headers[i].innerHTML = l10n[l10n_lang]["tbl_headers"][i];
-	}
-
-	restart_button = document.getElementById("restart");
-	restart_button.innerHTML = l10n[l10n_lang]["restart"];
+	restartButton = document.getElementById("restart");
+	if (restartButton) { restartButton.innerHTML = l10n["restart"] }
 
 	powered = document.getElementById("powered");
-	powered.innerHTML = l10n[l10n_lang]["powered"];
+	if (powered) { powered.innerHTML = l10n["powered"] }
 
-	does_not_exist = document.getElementById("does_not_exist");
-	does_not_exist.innerHTML = l10n[l10n_lang]["does_not_exist"];
+	doesNotExist = document.getElementById("does-not-exist");
+	if (doesNotExist) { doesNotExist.innerHTML = l10n["does_not_exist"] }
 
-	err_tags = document.getElementsByClassName("tag L2_err_tag is-primary");
-	if (err_tags.length > 0) {
-		err_tags[0].click();
-	}
-}
-
-
-isVisible = function (elm) {
-	var rect = elm.getBoundingClientRect();
-	var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-	return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-}
-
-
-click_err = function (elem) {
-	// alert(elem.dataset.errs);
-
-	err_tokens = document.getElementsByClassName("err");
-	for (var i = 0; i < err_tokens.length; i++) {
-		err_tokens[i].classList.remove("is-medium", "is-primary", "tag");
-		err_tokens[i].classList.add("has-text-link");
-	}
-	elem.classList.remove("has-text-link");
-	elem.classList.add("is-medium", "is-primary", "tag");
-
-	err_json = JSON.parse(elem.dataset.errs);
-	readings = '<span class="tag is-medium is-primary">' + elem.innerHTML + '</span> '
-	readings += '<span id="does_not_exist">' + l10n[l10n_lang]["does_not_exist"] + '</span>'
-	readings += '<table class="table"><tr>'
-	readings += '<th>' + l10n[l10n_lang]['tbl_headers'][0] + '</th>'
-	readings += '<th>' + l10n[l10n_lang]['tbl_headers'][1] + '</th>'
-	readings += '<th>' + l10n[l10n_lang]['tbl_headers'][2] + '</th>'
-	readings += '</tr>'
-	for (var i = 0; i < err_json.length; i++) {
-		r = err_json[i]
-		readings += "<tr>"
-		readings += "<td><span class=lemma>" + r.lemma + "</span></td> "
-		readings += "<td>"
-		for (var j = 0; j < r.L2_error_tags.length; j++) {
-			readings += '<span class="tag is-clickable is-link is-medium is-rounded L2_err_tag" onclick="fetch_and_load_err_html(this, \'' + r.L2_error_tags[j] + '\')">' + l10n[l10n_lang]["errors"][r.L2_error_tags[j]] + "</span> "
+	table = document.querySelector("table.error-list");
+	if (table) {
+		tokId = table.getAttribute("data-tok-id")
+		highlightedTag = table.getAttribute("data-highlighted-tag")
+		document.getElementById(tokId).click()
+		if (highlightedTag) {
+			htmx.ajax("GET", `/explanation/${newLang}/${highlightedTag}`, "#explanation").then(() => {highlightL2ErrTag(highlightedTag)})
 		}
-		readings += "</td>"
-		readings += '<td><button class="button is-info is-light"><span class="icon"><i class="fas fa-eye-slash"></i></span><span class="button is-info is-inverted is-light is-outlined">' + r.corrected + "</span></button></td>"
-		readings += "</tr>"
-	}
-	readings += "</table>"
-
-	err_readings_div = document.getElementById("error_readings");
-	err_readings_div.innerHTML = readings;
-
-	if (err_json.length == 1) {
-		if (err_json[0].L2_error_tags.length == 1) {
-			err_tags = err_readings_div.getElementsByClassName("L2_err_tag");
-			err_tags[0].click()
-		}
-	} else {
-		load_err_html("")
 	}
 }
 
-fetch_and_load_err_html = function (elem, err_id) {
-	err_tags = document.getElementsByClassName("L2_err_tag");
-	for (var i = 0; i < err_tags.length; i++) {
-		if (err_tags[i].innerHTML == elem.innerHTML) {
-			err_tags[i].classList.add("is-primary");
-			err_tags[i].classList.remove("is-link");
+
+highlightToken = function (elem) {
+  errTokens = document.getElementsByClassName("err is-medium is-primary tag");
+  for (var i in errTokens) {
+    errTokens[i]?.classList?.remove("is-medium", "is-primary", "tag");
+    errTokens[i]?.classList?.add("has-text-link");
+  }
+  elem?.classList?.remove("has-text-link");
+  elem?.classList?.add("is-medium", "is-primary", "tag");
+}
+
+
+highlightL2ErrTag = function (L2ErrName) {
+	errTags = document.getElementsByClassName("L2-err-tag");
+	for (i in errTags) {
+		if (errTags[i].classList?.contains(L2ErrName)) {
+			errTags[i].classList?.add("is-primary");
+			errTags[i].classList?.remove("is-link");
 		} else {
-			err_tags[i].classList.remove("is-primary");
-			err_tags[i].classList.add("is-link");
+			errTags[i].classList?.remove("is-primary");
+			errTags[i].classList?.add("is-link");
 		}
 	}
-
-	var response = fetch('https://reynoldsnlp.github.io/rus_grammar_explanations/html/' + l10n_lang + "/" + err_id + '.html')
-		.then(response => response.text())
-		.then(explanation_src => load_err_html(explanation_src));
-}
-
-load_err_html = function (src) {
-	var parser = new DOMParser();
-	var htmlDoc = parser.parseFromString(src, 'text/html');
-	var body = htmlDoc.getElementsByTagName("body");
-	var explanation_div = document.getElementById("explanation");
-	explanation_div.innerHTML = body[0].innerHTML;
-	if (explanation_div.innerHTML != "" && !isVisible(explanation_div)) {
-		explanation_div.scrollIntoView({ "behavior": "smooth" })
-	}
+	table = document.querySelector("table.error-list");
+	if (table) {table.setAttribute("data-highlighted-tag", L2ErrName)}
 }
